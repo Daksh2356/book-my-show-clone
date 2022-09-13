faeb16d41a90495c99f4a52e7bdc4bd5dbeb9a9e
Daksh2356 <dakshmakhija@gmail.com>
2022-09-13 20:27:40 +0530

    Completed HeroBanner on the main page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import "./App.css";
 
-//React slick-css
+// Routing
+import { Routes, Route } from "react-router-dom";
+
+// React slick-css
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
+// Pages
+import HomePage from "./pages/Home.Page";
+import MoviePage from "./pages/Movie.Page";
+import PlayPage from "./pages/Play.Page";
+
 function App() {
   return (
-    <div>
-      <h1 className="text-2xl font-bold px-3 py-3 underline">Hello world!</h1>
-    </div>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/movie/:id" element={<MoviePage />} />
+      <Route path="/plays" element={<PlayPage />} />
+    </Routes>
   );
 }
 
