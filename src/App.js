cab8785b13e8c9afecb7897a19aeb4582af1d813
Daksh2356 <dakshmakhija@gmail.com>
2022-09-13 15:15:24 +0530

    Initial Setup Completed

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+
+//React slick-css
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold px-3 py-3 underline">Hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
